1edac39cb3e569f19dda2120c7c2bedd6dbb9dd7
saoyuu <l>
2020-01-10 16:46:28 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      hello are you ok 
+    </div>
+  );
+}
+
+export default App;
